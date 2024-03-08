5c00d7bfb740e56babd5ab0b117385277c33fde6
wlsdks <wlsdks12@naver.com>
2024-03-08 14:59:35 +0900

    feat: 컨트롤러 작성

--- src/main/java/com/study/customannotation/controller/AnnotationController.java
+++ src/main/java/com/study/customannotation/controller/AnnotationController.java
@@ -1,4 +1,27 @@
 package com.study.customannotation.controller;
 
+import com.study.customannotation.annotation.JinanLogging;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AnnotationController {
+
+    @JinanLogging
+    @GetMapping("/customAnnotation")
+    public void customAnnotationMethod() {
+        System.out.println("커스텀 어노테이션 적용 메소드 실행");
+    }
+
+    @JinanLogging(message = "커스텀 어노테이션 메시지 직접 작성")
+    @GetMapping("/customAnnotationMethodSetMessage")
+    public void customAnnotationMethodSetMessage() {
+        System.out.println("커스텀 어노테이션 적용 메소드 실행");
+    }
+
+    @GetMapping("/nonAnnotation")
+    public void nonAnnotationMethod() {
+        System.out.println("커스텀 어노테이션 미적용 메소드 실행");
+    }
+
 }
